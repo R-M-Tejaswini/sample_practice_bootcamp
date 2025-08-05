d3fae07eb3f91fe67a03c207c0cd5c0492414e92
R.M.Tejaswini <tejaswini.rm@supporthive.com>
2025-08-05 14:19:53 +0530

    feat(hello): scaffold Django project and add /hello endpoint

--- greetings/urls.py
+++ greetings/urls.py
@@ -1,7 +1,7 @@
 # greetings/urls.py
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
-    path('hello/', views.hello, name='hello'),
+    path("hello/", views.hello, name="hello"),
 ]
--- greetings/views.py
+++ greetings/views.py
@@ -1,8 +1,10 @@
-#greetings/views.py
-#from django.shortcuts import render
+# greetings/views.py
+# from django.shortcuts import render
 from django.http import HttpResponse
+
 
 def hello(request):
     return HttpResponse("Hello, World!")
+
 
 # Create your views here.
